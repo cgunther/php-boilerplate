c07ba0057c421955228f86eb014fa3269539802d
Chris Gunther <chris@room118solutions.com>
2011-09-23 13:53:13 -0400

    upgrade to jQuery 1.6.4

--- layouts/application.php
+++ layouts/application.php
@@ -21,8 +21,8 @@
     <footer>
       
     </footer>
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="js/libs/jquery-1.6.3.min.js"><\/script>')</script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/libs/jquery-1.6.4.min.js"><\/script>')</script>
     <?php javascripts('script'); yield('javascripts'); ?>
 
     <script>
