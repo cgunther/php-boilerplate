2c2ff218e05b79203ae6bbbe539a832663f0b25b
Chris Gunther <chris@room118solutions.com>
2011-10-27 17:05:32 -0400

    include universal ie6 css

--- layouts/application.php
+++ layouts/application.php
@@ -7,8 +7,14 @@
     <meta charset="utf-8">
     <title><?php echo has_content('title') ? yield('title', false) . ' - ' : ''; ?>[SITE NAME]</title>
     <base href="<?php echo $config['base_url']; ?>">
-    <link rel="stylesheet" href="css/style.css">
-    <?php yield('stylesheets'); ?>
+    <!--[if ! lte IE 6]><!-->
+      <link rel="stylesheet" href="css/style.css">
+      <?php yield('stylesheets'); ?>
+    <!--<![endif]-->
+    <!--[if lte IE 6]>
+      <link rel="stylesheet" href="css/libs/ie6.1.1.css">
+      <style>html { padding-top: 42px; }</style>
+    <![endif]-->
     <script src="js/libs/modernizr-2.0.6.min.js"></script>
   </head>
   <body>
@@ -21,10 +27,13 @@
     <footer>
       
     </footer>
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="js/libs/jquery-1.6.4.min.js"><\/script>')</script>
-    <?php javascripts('script'); yield('javascripts'); ?>
-
+    
+    <!--[if ! lte IE 6]><!-->
+      <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+      <script>window.jQuery || document.write('<script src="js/libs/jquery-1.6.4.min.js"><\/script>')</script>
+      <?php javascripts('script'); yield('javascripts'); ?>
+    <!--<![endif]-->
+    
     <script>
       var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview'],['_trackPageLoadTime']];
       (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
@@ -38,7 +47,6 @@
           <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
         </a>
       </div>
-      <style>html { padding-top: 42px; }</style>
       <script defer src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
       <script defer>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
     <![endif]-->
